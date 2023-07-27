13aac7a7414e6eb13b4d3f1053122c18615a63dc
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-27 20:33:23 +0000

    fixed modal bug

--- food-app/src/components/Cart/Cart.js
+++ food-app/src/components/Cart/Cart.js
@@ -1,22 +1,28 @@
-import Modal from '../UI/Modal.JS';
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
-import Modal from '../UI/Modal.JS';
 
-const Cart = props => {
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
-    const cartItems = (<ul>{[{id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map((item) => (<li>{item.name}</li>))}</ul>);
-
-    return <Modal>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>€35.00</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}>Order</button>
-        </div>
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </Modal>
-}
+  );
+};
 
 export default Cart;
