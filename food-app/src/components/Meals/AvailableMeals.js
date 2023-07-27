b3fded574aa89a239bb7b75b00cabbdedf74963e
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-27 19:23:42 +0000

    add mealitem

--- food-app/src/components/Meals/AvailableMeals.js
+++ food-app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -25,17 +27,25 @@ const DUMMY_MEALS = [
       description: 'Healthy...and green...',
       price: 18.99,
     },
+    {
+      id: 'm5',
+      name: 'Curry',
+      description: 'Healthy...and green...',
+      price: 20.99,
+    },
   ];
 
 const AvailableMeals = () => {
 
-    const MealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const MealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>);
 
     return (
         <section className={classes.meals}>
+          <Card>
             <ul>
                 {MealsList}
             </ul>
+          </Card>
         </section>
     )
 }
