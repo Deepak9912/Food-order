66cb3401ebdc6b682891ec49fc09ea3081b8aee8
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-28 19:09:16 +0000

    add modal working

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  // using useState to manage the cart
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {/* cart will be rendered contiditionally so we will wrap it in {} and use is statement */}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
